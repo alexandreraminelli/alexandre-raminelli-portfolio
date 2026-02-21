a61a5d4314627d0ed1349512435798e9c8aa2192
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-02-21 18:22:04 -0300

    feat(metadata): configurar metadados do site

---
+++ src/constants/content/contact.ts
@@ -0,0 +1,8 @@
+/**
+ * Informações de contato para exibição no site.
+ */
+export const CONTACT = {
+  email: "alexandre.raminelli9491@gmail.com",
+  github: "https://github.com/alexandreraminelli",
+  linkedin: "https://www.linkedin.com/in/alexandre-raminelli/",
+}
